73d3dc750e4c9650050e0620b1ff8c7a4410e825
truji9 <truji_9@hotmail.com>
2016-03-13 12:47:15 +0100

    Matriz creada correctamente

---
+++ src/packCodigo/Pruebas.java
@@ -0,0 +1,12 @@
+package packCodigo;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Buscaminas.getBuscaminas().inicioJuego(2);
+		Buscaminas.getBuscaminas().imprimirPorConsola();
+	}
+
+}
