8db7d94eaec609dad00772ecee7546e0f2ad0156
peiov_000 <peiov_000@10.106.24.74>
2016-04-07 09:38:45 +0200

    un test

--- src/packJUnit/CasillaTest.java
+++ src/packJUnit/CasillaTest.java
@@ -6,54 +6,74 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import packCodigo.Casilla;
+import packCodigo.CasillaFactory;
+
 public class CasillaTest {
 
+	private Casilla cV;
+	private Casilla cM;
+	private Casilla cN;
+	private CasillaFactory f;
+	
 	@Before
 	public void setUp() throws Exception {
+		cV = f.getMiFactoria().generarCasilla("Vacia");
+		cM = f.getMiFactoria().generarCasilla("Mina");
+		cN = f.getMiFactoria().generarCasilla("Numero");
+		cV.inicializar("0,1");
+		cM.inicializar("0,0");
+		cN.inicializar("1,1");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		cV = null;
+		cM = null;
+		cN = null;
 	}
 
 	@Test
 	public void testCasilla() {
-		fail("Not yet implemented"); // TODO
+		assertNotNull(cV);
+		assertNotNull(cM);
+		assertNotNull(cN);
 	}
 
 	@Test
 	public void testObtenerCoordenadas() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testDescubrir() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testInicializar() {
-		fail("Not yet implemented"); // TODO
+		assertTrue(cV.obtenerCoordenadas()=="0,1");
+		assertTrue(cM.obtenerCoordenadas()=="0,0");
+		assertTrue(cN.obtenerCoordenadas()=="1,1");
 	}
 
 	@Test
 	public void testCambioBandera() {
-		fail("Not yet implemented"); // TODO
+		cV.cambioBandera();
+		assertTrue(cV.tieneBandera());
+		cV.cambioBandera();
+		assertFalse(cV.tieneBandera());
+		cV.descubrir();
+		assertFalse(cV.tieneBandera());
+		cM.cambioBandera();
+		assertTrue(cM.tieneBandera());
+		cM.cambioBandera();
+		assertFalse(cM.tieneBandera());
+		cM.descubrir();
+		assertFalse(cM.tieneBandera());
+		cN.cambioBandera();
+		assertTrue(cN.tieneBandera());
+		cN.cambioBandera();
+		assertFalse(cN.tieneBandera());
+		cN.descubrir();
+		assertFalse(cN.tieneBandera());
+		
 	}
 
 	@Test
 	public void testEstaDesvelada() {
-		fail("Not yet implemented"); // TODO
+		cV.imprimirInfo();
+		cM.imprimirInfo();
+		cN.imprimirInfo();
 	}
-
-	@Test
-	public void testTieneBandera() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testImprimirInfo() {
-		fail("Not yet implemented"); // TODO
-	}
-
 }
