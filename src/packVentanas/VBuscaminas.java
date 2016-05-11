9eb744655e73264f9cf7e9d27f4e20db796f406c
peiov_000 <peiov_000@10.106.20.4>
2016-05-11 12:26:10 +0200

    music

--- src/packVentanas/VBuscaminas.java
+++ src/packVentanas/VBuscaminas.java
@@ -357,8 +357,8 @@ public class VBuscaminas extends JFrame implements ActionListener, Observer{
 				    }else if(Integer.parseInt(p[2])==11){
 				    	lcasillas[pos].setIcon(new ImageIcon(VBuscaminas.class.getResource("/CasillaNoMina.png")));
 				    }
+				}
 			}
-		}
 	}
 	
 
@@ -410,9 +410,9 @@ public class VBuscaminas extends JFrame implements ActionListener, Observer{
 	
 	private void play(boolean pB) throws NoArchivoAudioException{
 		if (pB==false){
-			if (new File("sources/login.wav").getAbsoluteFile() != null){
+			if (new File("sources/lose.wav").getAbsoluteFile() != null){
 				try {
-					ais = AudioSystem.getAudioInputStream(new File("sources/login.wav").getAbsoluteFile());
+					ais = AudioSystem.getAudioInputStream(new File("sources/lose.wav").getAbsoluteFile());
 				} catch (UnsupportedAudioFileException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
@@ -458,5 +458,6 @@ public class VBuscaminas extends JFrame implements ActionListener, Observer{
 				throw new NoArchivoAudioException();
 			}
 		}
+		clip.start();
 	}
 }
